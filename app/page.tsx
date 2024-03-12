55900ca3d63ba1ca5f2b7c916bed84580029d873
daklik-not <0987azaaz65@gmail.com>
2024-03-12 22:58:23 +0300

    ASDAD

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,10 @@
 export default function Home() {
   return (
     <>
-    <Bebra/>
-    </>
-  );
-}
-
-export function Bebra(){
-  return(
-    <>
     <div className="flex">
       <p className="text-[450px]">БЕБРА</p>
     </div>
 
     </>
-  )
+  );
 }
